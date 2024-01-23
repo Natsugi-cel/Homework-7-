ee131dcfccf3f1e309bdeb7dbcd4a650ad3606c9
kolom <s_kolomeetzc@ukr.net>
2024-01-23 21:09:26 +0100

    Homework 7, begin 2 end

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 print("Hello world")
 
 ######### 1 #########
-# v1
-def bigmult(*numbers):
-    n = 1
-    for i in numbers:
-        n *= i
-    return n
-
-print(bigmult(1, 2, 3))
-
-# v2
-def multiply_elements(lst):
-    result = 1
-for element in lst:
-    result *= element
-    return result
+# # v1
+# def bigmult(*numbers):
+#     n = 1
+#     for i in numbers:
+#         n *= i
+#     return n
+#
+# print(bigmult(1, 2, 3))
+#
+# # v2
+# def multiply_elements(lst):
+#     result = 1
+# for element in lst:
+#     result *= element
+#     return result
 
 ############ 2 ################
 
+def find_min(mass):
+   elem_min = mass[0]
+   for elem in mass:
+       if elem < elem_min:
+           elem_min = elem
+   return elem_min
+mass = [int(s) for s in input().split()]
+print(find_min(mass))
