cb486082603a756b003396523cd4c25f7ea76b63
kolom <s_kolomeetzc@ukr.net>
2024-01-23 21:05:26 +0100

    Homework 7, begin 1 end

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+######### 1 #########
+# v1
+def bigmult(*numbers):
+    n = 1
+    for i in numbers:
+        n *= i
+    return n
+
+print(bigmult(1, 2, 3))
+
+# v2
+def multiply_elements(lst):
+    result = 1
+for element in lst:
+    result *= element
+    return result
